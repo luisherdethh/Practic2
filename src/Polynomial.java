98df63a51d079709650d943ef5d23b1f8ace8145
Luis Herdeth Ubuntu <lh.mendozav@gmail.com>
2017-02-09 16:18:27 +0100

    Initial commit

---
+++ src/Polynomial.java
@@ -0,0 +1,58 @@
+import java.util.Arrays;
+
+public class Polynomial {
+
+    private float[] Poly = {0};
+
+    // Constructor per defecte. Genera un polinomi zero
+    public Polynomial() {
+
+    }
+
+    // Constructor a partir dels coeficients del polinomi en forma d'array
+    public Polynomial(float[] cfs) {
+        float prueba [] = new float[cfs.length];
+        for (int i=0, j=cfs.length-1; i>j; i++, j--){
+
+        }
+
+        System.out.println(Arrays.toString(this.Poly));
+    }
+
+    // Constructor a partir d'un string
+    public Polynomial(String s) {
+    }
+
+    // Suma el polinomi amb un altre. No modifica el polinomi actual (this). Genera un de nou
+    public Polynomial add(Polynomial p) {
+        return null;
+    }
+
+    // Multiplica el polinomi amb un altre. No modifica el polinomi actual (this). Genera un de nou
+    public Polynomial mult(Polynomial p2) {
+        return null;
+    }
+
+    // Divideix el polinomi amb un altre. No modifica el polinomi actual (this). Genera un de nou
+    // Torna el quocient i també el residu (ambdós polinomis)
+    public Polynomial[] div(Polynomial p2) {
+       return null;
+    }
+
+    // Troba les arrels del polinomi, ordenades de menor a major
+    public float[] roots() {
+        return null;
+    }
+
+    // Torna "true" si els polinomis són iguals. Això és un override d'un mètode de la classe Object
+    @Override
+    public boolean equals(Object o) {
+        return false;
+    }
+
+    // Torna la representació en forma de String del polinomi. Override d'un mètode de la classe Object
+    @Override
+    public String toString() {
+        return "";
+    }
+}
---
+++ src/Prueba.java
@@ -0,0 +1,10 @@
+/**
+ * Created by herdeth on 1/02/17.
+ */
+public class Prueba {
+    public static void main(String[] args) {
+        float[] pepe = {1,3,5};
+        //Polynomial p = new Polynomial(pepe);
+        System.out.println(pepe);
+    }
+}
